0579dd17bd61ce3109d5b25b7d89ec6a25876a63
George Mamadashvili <georgemamadashvili@gmail.com>
2020-12-04 11:14:27 +0400

    MVP

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,12 +2,12 @@
 
 namespace Sortarad\Ghostwriter;
 
-use Sortarad\Ghostwriter\Tags\Ghostwriter;
+use Sortarad\Ghostwriter\Tags\GhostwriterTags;
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
     protected $tags = [
-        Ghostwriter::class,
+        GhostwriterTags::class,
     ];
 }
---
+++ src/Tags/GhostwriterTags.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sortarad\Ghostwriter\Tags;
+
+use Statamic\Tags\Tags;
+
+class GhostwriterTags extends Tags
+{
+    protected static $handle = 'ghostwriter';
+
+    /**
+     * The {{ ghostwriter }} tag.
+     *
+     * @return string|array
+     */
+    public function index()
+    {
+        $options = [
+            'typeSpeed' => $this->params->int('typeSpeed', 40),
+            'loop' => $this->params->bool('loop'),
+        ];
+        $json = json_encode( $options, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT );
+
+        return sprintf("data-ghostwriter='%s'", $json );
+    }
+
+    /**
+     * The {{ ghostwriter:assets }} tag.
+     *
+     * @return string|array
+     */
+    public function assets()
+    {
+        return view('ghostwriter::assets');
+    }
+}
