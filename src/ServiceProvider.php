c233487b7219fc3938ede73d23b6743cf02d32e9
George Mamadashvili <georgemamadashvili@gmail.com>
2020-12-08 14:41:32 +0400

    Manually publish add-on publishables before the issue is fixed

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Sortarad\Ghostwriter;
 
+use Statamic\Statamic;
 use Sortarad\Ghostwriter\Tags\GhostwriterTags;
 use Statamic\Providers\AddonServiceProvider;
 
@@ -14,4 +15,31 @@ class ServiceProvider extends AddonServiceProvider
     protected $publishables = [
         __DIR__.'/../resources/dist' => '',
     ];
+
+    public function boot()
+    {
+        parent::boot();
+
+        $this->bootAddonPublishAfterInstall();
+    }
+
+    public function bootAddonPublishAfterInstall()
+    {
+        if (! $this->publishAfterInstall) {
+            return $this;
+        }
+
+        if (empty($this->publishables)) {
+            return $this;
+        }
+
+        Statamic::afterInstalled(function ($command) {
+            $command->call('vendor:publish', [
+                '--tag' => $this->getAddon()->slug(),
+                '--force' => true,
+            ]);
+        });
+
+        return $this;
+    }
 }
